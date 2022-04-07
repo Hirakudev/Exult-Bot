781ac2638cebc1d746a38136214fb4aee1a65c99
Andy <andrewg.mason2709@gmail.com>
2022-04-07 04:35:03 +0100

    Added custom app_command checks

--- bot/utils/__init__.py
+++ bot/utils/__init__.py
@@ -1,3 +1,4 @@
-from .ic import *
-from .helpers import *
-from .checks import *
+from .ic import ExultBot, ExultCog
+from .helpers import embed_builder, time_handler
+from .checks import permissions, guild_staff
+from .database import CasesDB, GuildsDB, LogsDB
--- bot/utils/checks.py
+++ bot/utils/checks.py
@@ -1,6 +1,12 @@
 from discord import Interaction
 from discord.app_commands import check, MissingPermissions, BotMissingPermissions
 from discord import Permissions
+#Discord Imports
+
+from .database import GuildsDB
+
+async def is_owner(itr: Interaction):
+    return await itr.client.is_owner(itr.user)
 
 def permissions(**perms: bool):
     user_invalid = perms.keys() - Permissions.VALID_FLAGS.keys()
@@ -9,16 +15,8 @@ def permissions(**perms: bool):
         raise TypeError(f"Invalid user permission(s): {', '.join(user_invalid)}")
     if bot_invalid:
         raise TypeError(f"Invalid bot permission(s): {', '.join(bot_invalid)}")
-    def predicate(itr: Interaction):
+    async def predicate(itr: Interaction):
         final_result = [False, False]
-        #See if user is able to run command.
-        permissions = itr.permissions
-        missing = [perm for perm, value in perms.items() if getattr(permissions, perm) != value]
-        if not missing:
-            final_result[0] = True
-        else:
-            raise MissingPermissions(missing)
-
         #See if bot is able to carry out command.
         me = itr.guild.me if itr.guild is not None else itr.client.user
         if itr.channel is None:
@@ -33,7 +31,35 @@ def permissions(**perms: bool):
         else:
             raise BotMissingPermissions(missing)
 
+        if await is_owner(itr):
+            return True
+
+        #See if user is able to run command.
+        permissions = itr.permissions
+        missing = [perm for perm, value in perms.items() if getattr(permissions, perm) != value]
+        if not missing:
+            final_result[0] = True
+        else:
+            raise MissingPermissions(missing)
+
         if final_result[0] and final_result[1]:
             return True
 
     return check(predicate)
+
+def guild_staff():
+    async def predicate(itr: Interaction):
+        if await is_owner(itr):
+            return True
+        staff_roles = await GuildsDB(itr.client).get_staff_roles(itr.guild.id)
+        staff_users = await GuildsDB(itr.client).get_staff(itr.guild.id)
+        if itr.user in staff_users["moderator_users"] or itr.user in staff_users["admin_users"]:
+            return True
+        for role in itr.user.roles:
+            if role.id in staff_roles["moderator_roles"]:
+                return True
+            elif role.id in staff_roles["admin_roles"]:
+                return True
+
+    return check(predicate)
+        
