2c5353d913b3a4c9089a2b3323147c484a7a2979
Andy <andrewg.mason2709@gmail.com>
2022-04-07 04:34:20 +0100

    Time helper classes

--- bot/utils/helpers.py
+++ bot/utils/helpers.py
@@ -1,7 +1,13 @@
-from discord import Embed
+from discord import Embed, Interaction
+from discord.ext.commands import BadArgument, Context
 #Discord Imports
 
 from typing import Union
+import re
+from dateutil.relativedelta import relativedelta
+import parsedatetime as pdt
+import datetime
+import pytz
 #Regular Imports
 
 from .ic.bot import ExultBot
@@ -36,3 +42,82 @@ def embed_builder(*, title:Union[str, None]=None, description:Union[str, None]=N
             except IndexError:
                 embed.add_field(name=field[0], value=field[1])
     return embed
+
+class ShortTime:
+    compiled = re.compile("""(?:(?P<years>[0-9])(?:years?|y))?             # e.g. 2y
+                             (?:(?P<months>[0-9]{1,2})(?:months?|mo))?     # e.g. 2months
+                             (?:(?P<weeks>[0-9]{1,4})(?:weeks?|w))?        # e.g. 10w
+                             (?:(?P<days>[0-9]{1,5})(?:days?|d))?          # e.g. 14d
+                             (?:(?P<hours>[0-9]{1,5})(?:hours?|h))?        # e.g. 12h
+                             (?:(?P<minutes>[0-9]{1,5})(?:minutes?|m))?    # e.g. 10m
+                             (?:(?P<seconds>[0-9]{1,5})(?:seconds?|s))?    # e.g. 15s
+                          """, re.VERBOSE)
+
+    def __init__(self, argument, *, now=None):
+        match = self.compiled.fullmatch(argument)
+        if match is None or not match.group(0):
+            raise BadArgument('invalid time provided')
+
+        data = { k: int(v) for k, v in match.groupdict(default=0).items() }
+        now = now or datetime.datetime.now(datetime.timezone.utc)
+        self.dt = now + relativedelta(**data)
+
+    @classmethod
+    async def convert(cls, ctx, argument):
+        return cls(argument, now=ctx.message.created_at)
+
+class HumanTime:
+    calendar = pdt.Calendar(version=pdt.VERSION_CONTEXT_STYLE)
+
+    def __init__(self, argument, *, now=None):
+        now = now or datetime.datetime.utcnow()
+        dt, status = self.calendar.parseDT(argument, sourceTime=now)
+        if not status.hasDateOrTime:
+            raise BadArgument("Make sure you format your duration correctly! `(e.g. 5 hours 30 minutes)`")
+        
+        if not status.hasTime:
+            dt = dt.replace(hour=now.hour, minute=now.minute, second=now.second, microsecond=now.microsecond)
+
+        self.dt = dt
+        self._past = dt < now
+
+    @classmethod
+    async def convert(cls, ctx, argument):
+        if isinstance(ctx, Interaction):
+            return cls(argument, now=ctx.created_at)
+        elif isinstance(ctx, Context):
+            return cls(argument, now=ctx.message.created_at)
+
+class Time(HumanTime):
+    def __init__(self, argument, *, now=None):
+        try:
+            o = ShortTime(argument, now=now)
+        except Exception as e:
+            super().__init__(argument)
+        else:
+            self.dt = o.dt
+            self._past = False
+
+class FutureTime(Time):
+    def __init__(self, argument, *, now=None):
+        argument_list = list(argument)
+        if " " not in argument_list:
+            pos = -1
+            for c in argument_list:
+                pos += 1
+                if str(c).isalpha():
+                    break
+            argument_list.insert(pos, " ")
+            argument = "".join([z for z in argument_list])
+        super().__init__(argument, now=now)
+
+        if self._past:
+            raise BadArgument("This time is in the past.")
+
+def time_handler(time) -> datetime.datetime:
+    time = FutureTime(time).dt
+    try:
+        localised = ExultBot.time(time)
+    except:
+        raise BadArgument("Make sure you format your duration correctly! `(e.g. 5 hours 30 minutes)`")
+    return localised
