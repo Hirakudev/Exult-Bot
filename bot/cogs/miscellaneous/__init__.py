46bf739d338a7ae705bbda517a13dbdf47ce6352
VarMonke <var.monke@gmail.com>
2022-04-11 20:29:51 +0530

    Fix cog(dumb)

--- bot/cogs/miscellaneous/__init__.py
+++ bot/cogs/miscellaneous/__init__.py
@@ -1,14 +1,14 @@
 from discord import Object
 #Discord Imports
 
-
+from .miscellaneous import Miscellaneous
 #Cog Imports
 
 from utils import *
 #Local Imports
 
 
-class Miscellaneous(ExultCog):
+class Miscellaneous(Miscellaneous):
     """ Miscellaneous Cog """
 
 async def setup(bot: ExultBot):
---
+++ bot/cogs/miscellaneous/miscellaneous.py
@@ -0,0 +1,12 @@
+from discord import Interaction, Member, Webhook, Role
+from discord.app_commands import describe, Group
+# Discord Imports
+
+#Regular Imports
+
+from utils import *
+# Local Imports
+
+class Miscellaneous(ExultCog):
+    pass
+
