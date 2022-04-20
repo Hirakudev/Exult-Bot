e7b8255bf29bbcfad7a3cfedf3bff45ec0e3487f
Var <94229714+VarMonke@users.noreply.github.com>
2022-04-20 17:01:32 +0530

    Update temp.py to bug fix 80% fills

--- temp.py
+++ temp.py
@@ -1,12 +1,10 @@
-[Temp PIL functions]
-import asyncio
-from io import BytesIO
-from typing import (Union)
-
 from PIL import Image, ImageDraw, ImageOps
 import aiohttp
 from easy_pil import Editor, Canvas, Font
 import discord
+from typing import Union
+from io import BytesIO
+bot.remove_command('uwu')
 @bot.command(name='uwu',)
 async def rankcard(ctx, rank, level, xp, xp_total):
   async def get_avatar(user: discord.Member = ctx.author):
@@ -27,7 +25,7 @@ async def rankcard(ctx, rank, level, xp, xp_total):
           draw.rounded_rectangle((25, 25, 1055, 375), outline="#fb5f5f", width=5, radius=35)
           draw.rounded_rectangle((490, 240, 958, 300), outline="#ffffff", width=5, radius=100)
           background.rounded_corners(radius=50)
-          
+
           background.text(position=(825, 210), text=f"{xp} / {xp_total} XP", font=font_small, color="#ffffff")
           background.text(position=(500,205),text=str(data[1]),font=font_text,color="#9D9D9E")
           background.paste(image, (50, 50))
@@ -36,13 +34,12 @@ async def rankcard(ctx, rank, level, xp, xp_total):
           background.text(position=(800,75),text=f"Level",color="#ffffff",font=font)
           background.text(position=(900,70),text=f" {level}",color="#fb5f5f",font=font_big)
           percent = round((xp/xp_total)*100)
-          background.bar((500, 250),max_width=650,height=40,percentage=int(percent),fill="#fb5f5f",radius=30,)
+          background.bar((500, 250),max_width=450,height=40,percentage=int(percent),fill="#fb5f5f",radius=30,)
           buffer = BytesIO()
-          background.image = background.image.resize(size = (background.image.size),resample=Image.ANTIALIAS)
           background.image.save(buffer, format="PNG")
           buffer.seek(0)
           return buffer
-        
+
   data = await get_avatar(ctx.author)
   fp = rank_card(data=data,level=level,rank=rank,xp=int(xp),xp_total=int(xp_total))
   await ctx.send(file=discord.File(fp=fp,filename='rank.png'))
