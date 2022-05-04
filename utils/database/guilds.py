da5ed9fe9a1548b5d097be55403039e555e3b081
Andy <andrewg.mason2709@gmail.com>
2022-05-04 11:52:51 +0100

    Added database transaction context managers

--- utils/database/guilds.py
+++ utils/database/guilds.py
@@ -6,31 +6,37 @@ from ._core import CoreDB
 class GuildsDB(CoreDB):
     async def add_guild(self, guild_id: int):
         async with self.pool.acquire() as conn:
-            await conn.execute("INSERT INTO guilds (guild_id) VALUES ($1)", guild_id)
+            async with conn.transaction():
+                await conn.execute(
+                    "INSERT INTO guilds (guild_id) VALUES ($1)", guild_id
+                )
 
     async def remove_guild(self, guild_id: int):
         async with self.pool.acquire() as conn:
-            await conn.execute("DELETE FROM guilds WHERE guild_id=$1", guild_id)
+            async with conn.transaction():
+                await conn.execute("DELETE FROM guilds WHERE guild_id=$1", guild_id)
 
     async def get_staff_roles(self, guild_id: int):
         async with self.pool.acquire() as conn:
-            roles = dict(
-                await conn.fetchrow(
+            async with conn.transaction():
+                roles = await conn.fetchrow(
                     "SELECT moderator_roles, admin_roles FROM guilds WHERE guild_id=$1",
                     guild_id,
                 )
-            )
-            return roles
+                if roles:
+                    return dict(roles)
+        return None
 
     async def get_staff(self, guild_id: int):
         async with self.pool.acquire() as conn:
-            users = dict(
-                await conn.fetchrow(
+            async with conn.transaction():
+                users = await conn.fetchrow(
                     "SELECT moderator_users, admin_users FROM guilds WHERE guild_id=$1",
                     guild_id,
                 )
-            )
-            return users
+                if users:
+                    return dict(users)
+        return None
 
     async def add_moderator_role(self, guild_id: int, role_id: int):
         roles = await self.get_staff_roles(guild_id)
@@ -38,12 +44,13 @@ class GuildsDB(CoreDB):
             return False
         roles["moderator_roles"].append(role_id)
         async with self.pool.acquire() as conn:
-            await conn.execute(
-                "UPDATE guilds SET moderator_roles=$1 WHERE guild_id=$2",
-                roles["moderator_roles"],
-                guild_id,
-            )
-        return True
+            async with conn.transaction():
+                await conn.execute(
+                    "UPDATE guilds SET moderator_roles=$1 WHERE guild_id=$2",
+                    roles["moderator_roles"],
+                    guild_id,
+                )
+                return True
 
     async def add_moderator_user(self, guild_id: int, user_id: int):
         users = await self.get_staff(guild_id)
@@ -51,12 +58,13 @@ class GuildsDB(CoreDB):
             return False
         users["moderator_users"].append(user_id)
         async with self.pool.acquire() as conn:
-            await conn.execute(
-                "UPDATE guilds SET moderator_users=$1 WHERE guild_id=$2",
-                users["moderator_users"],
-                guild_id,
-            )
-        return True
+            async with conn.transaction():
+                await conn.execute(
+                    "UPDATE guilds SET moderator_users=$1 WHERE guild_id=$2",
+                    users["moderator_users"],
+                    guild_id,
+                )
+                return True
 
     async def add_admin_role(self, guild_id: int, role_id: int):
         roles = await self.get_staff_roles(guild_id)
@@ -64,12 +72,13 @@ class GuildsDB(CoreDB):
             return False
         roles["admin_roles"].append(role_id)
         async with self.pool.acquire() as conn:
-            await conn.execute(
-                "UPDATE guilds SET admin_roles=$1 WHERE guild_id=$2",
-                roles["admin_roles"],
-                guild_id,
-            )
-        return True
+            async with conn.transaction():
+                await conn.execute(
+                    "UPDATE guilds SET admin_roles=$1 WHERE guild_id=$2",
+                    roles["admin_roles"],
+                    guild_id,
+                )
+                return True
 
     async def add_admin_user(self, guild_id: int, user_id: int):
         users = await self.get_staff(guild_id)
@@ -77,12 +86,13 @@ class GuildsDB(CoreDB):
             return False
         users["admin_users"].append(user_id)
         async with self.pool.acquire() as conn:
-            await conn.execute(
-                "UPDATE guilds SET moderator_users=$1 WHERE guild_id=$2",
-                users["admin_users"],
-                guild_id,
-            )
-        return True
+            async with conn.transaction():
+                await conn.execute(
+                    "UPDATE guilds SET moderator_users=$1 WHERE guild_id=$2",
+                    users["admin_users"],
+                    guild_id,
+                )
+                return True
 
     async def remove_moderator_role(self, guild_id: int, role_id: int):
         roles = await self.get_staff_roles(guild_id)
@@ -90,12 +100,13 @@ class GuildsDB(CoreDB):
             return False
         roles["moderator_roles"].remove(role_id)
         async with self.pool.acquire() as conn:
-            await conn.execute(
-                "UPDATE guilds SET moderator_roles=$1 WHERE guild_id=$2",
-                roles["moderator_roles"],
-                guild_id,
-            )
-        return True
+            async with conn.transaction():
+                await conn.execute(
+                    "UPDATE guilds SET moderator_roles=$1 WHERE guild_id=$2",
+                    roles["moderator_roles"],
+                    guild_id,
+                )
+                return True
 
     async def remove_moderator_user(self, guild_id: int, user_id: int):
         users = await self.get_staff(guild_id)
@@ -103,12 +114,13 @@ class GuildsDB(CoreDB):
             return False
         users["moderator_users"].remove(user_id)
         async with self.pool.acquire() as conn:
-            await conn.execute(
-                "UPDATE guilds SET moderator_users=$1 WHERE guild_id=$2",
-                users["moderator_users"],
-                guild_id,
-            )
-        return True
+            async with conn.transaction():
+                await conn.execute(
+                    "UPDATE guilds SET moderator_users=$1 WHERE guild_id=$2",
+                    users["moderator_users"],
+                    guild_id,
+                )
+                return True
 
     async def remove_admin_role(self, guild_id: int, role_id: int):
         roles = await self.get_staff_roles(guild_id)
@@ -116,12 +128,13 @@ class GuildsDB(CoreDB):
             return False
         roles["admin_roles"].remove(role_id)
         async with self.pool.acquire() as conn:
-            await conn.execute(
-                "UPDATE guilds SET admin_roles=$1 WHERE guild_id=$2",
-                roles["admin_roles"],
-                guild_id,
-            )
-        return True
+            async with conn.transaction():
+                await conn.execute(
+                    "UPDATE guilds SET admin_roles=$1 WHERE guild_id=$2",
+                    roles["admin_roles"],
+                    guild_id,
+                )
+                return True
 
     async def remove_admin_user(self, guild_id: int, user_id: int):
         users = await self.get_staff(guild_id)
@@ -129,9 +142,10 @@ class GuildsDB(CoreDB):
             return False
         users["admin_users"].remove(user_id)
         async with self.pool.acquire() as conn:
-            await conn.execute(
-                "UPDATE guilds SET moderator_users=$1 WHERE guild_id=$2",
-                users["admin_users"],
-                guild_id,
-            )
-        return True
+            async with conn.transaction():
+                await conn.execute(
+                    "UPDATE guilds SET moderator_users=$1 WHERE guild_id=$2",
+                    users["admin_users"],
+                    guild_id,
+                )
+                return True
--- utils/database/logs.py
+++ utils/database/logs.py
@@ -6,7 +6,10 @@ from ._core import CoreDB
 class LogsDB(CoreDB):
     async def get_moderation_logs(self, guild_id):
         async with self.pool.acquire() as conn:
-            log = await conn.fetchval(
-                "SELECT moderation_logs FROM log_config WHERE guild_id=$1", guild_id
-            )
-            return log
+            async with conn.transaction():
+                log = await conn.fetchval(
+                    "SELECT moderation_logs FROM log_config WHERE guild_id=$1", guild_id
+                )
+                if log:
+                    return log
+        return None
