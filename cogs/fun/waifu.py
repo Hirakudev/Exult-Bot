18f327cd421fb7ee6321a39c048d0071af1ea3f8
Andy <andrewg.mason2709@gmail.com>
2022-05-23 04:36:22 +0100

    Improved waifu cog

--- cogs/fun/waifu.py
+++ cogs/fun/waifu.py
@@ -14,23 +14,23 @@ class Waifu(commands.Cog):
     def __init__(self, bot: ExultBot):
         self.bot = bot
 
-    sfw_tags = [
-        "maid",
-        "marin-kitagawa",
-        "mori-calliope",
-        "oppai",
-        "raiden-shogun",
-        "selfies",
-        "uniform",
-        "waifu",
-    ]
-    nsfw_tags = ["ass", "hentai", "milf", "oral", "paizuri" "ecchi", "ero"] + sfw_tags
+    async def cog_load(self):
+        endpoints = await self.bot.wf.endpoints()
+        self.versatile_tags = ["andeh"] + endpoints.get("versatile")
+        self.nsfw_tags = endpoints.get("nsfw") + endpoints.get("versatile")
+
+    andeh = ["https://i.imgur.com/cZvj1Ws.png", "https://i.imgur.com/Mlz5Iri.png"]
 
     def waifu_embed(self, user: discord.Member, tag: str, image: str):
+        name = (
+            f" {tag.replace('waifu', 'random').capitalize()} "
+            if tag != "andeh"
+            else " "
+        )
         embed = embed_builder(
             author=[
                 self.bot.try_asset(user.avatar, user.default_avatar),
-                f"{user.name.capitalize()}'s {tag.replace('waifu', 'random').capitalize()} Waifu",
+                f"{user.name.capitalize()}'s{name}Waifu",
             ],
             image=image,
         )
@@ -40,12 +40,16 @@ class Waifu(commands.Cog):
 
     @waifu.command(name="sfw", description="Get an SFW Waifu Image")
     async def waifu_sfw_slash(self, itr: discord.Interaction, tag: str):
-        if tag not in self.sfw_tags:
+        if tag not in self.versatile_tags:
             return await itr.response.send_message(
                 f"`{tag}` is not a valid tag! Try again with one of the tags in the autocomplete menu."
             )
         await itr.response.defer()
-        image = await self.bot.wf.random(selected_tags=[tag], is_nsfw=False)
+        image = (
+            random.choice(self.andeh)
+            if tag == "andeh"
+            else await self.bot.wf.random(selected_tags=[tag], is_nsfw=False)
+        )
         embed = self.waifu_embed(itr.user, tag, image)
         await itr.followup.send(embed=embed)
 
@@ -55,7 +59,7 @@ class Waifu(commands.Cog):
             return await itr.response.send_message(
                 f"This is not an nsfw channel!", ephemeral=True
             )
-        elif tag not in self.sfw_tags or tag not in self.nsfw_tags:
+        elif tag not in self.versatile_tags or tag not in self.nsfw_tags:
             await itr.response.send_message(
                 f"`{tag}` is not a valid tag! Try again with one of the tags in the autocomplete menu."
             )
@@ -66,14 +70,14 @@ class Waifu(commands.Cog):
 
     @waifu_sfw_slash.autocomplete("tag")
     async def waifu_sfw_autocomplete(self, itr: discord.Interaction, current: str):
-        sfw_tags = [
+        versatile_tags = [
             w.replace("-", " ").replace("waifu", "random").title()
-            for w in self.sfw_tags
+            for w in self.versatile_tags
         ]
         num = 0
         choices = []
-        for w in sfw_tags:
-            choices.append((sfw_tags[num], self.sfw_tags[num]))
+        for w in versatile_tags:
+            choices.append((versatile_tags[num], self.versatile_tags[num]))
             num += 1
         return [
             app_commands.Choice(name=c[0], value=c[1])
@@ -86,6 +90,7 @@ class Waifu(commands.Cog):
         nsfw_tags = [
             w.replace("-", " ").replace("waifu", "random").title()
             for w in self.nsfw_tags
+            if w != "andeh"
         ]
         choices = []
         num = 0
