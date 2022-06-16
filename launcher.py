d906936323b021106ee65e714410e0181e97093e
Andy <andrewg.mason2709@gmail.com>
2022-06-16 04:01:26 +0100

    add check for "test" command-line arg

--- launcher.py
+++ launcher.py
@@ -4,6 +4,7 @@ import aiohttp
 import asyncpg
 import asyncio
 import kimochi
+import sys
 
 # Regular Imports
 
@@ -13,18 +14,20 @@ from utils import *
 # Local Imports
 
 dotenv.load_dotenv()
+TOKEN = "TEST_TOKEN" if "test" in sys.argv else "TOKEN"
+PSQL_URI = "TEST_PSQL_URI" if "test" in sys.argv else "PSQL_URI"
 
 
 async def main():
     async with aiohttp.ClientSession() as session, asyncpg.create_pool(
-        os.environ["TEST_PSQL_URI"]
+        os.environ[PSQL_URI]
     ) as pool, pool.acquire() as listener_connection, ExultBot(
         session=session,
         pool=pool,
         listener_connection=listener_connection,
         kimochi_client=kimochi.Client(session),
     ) as bot:
-        await bot.start(os.environ["TEST_TOKEN"])
+        await bot.start(os.environ[TOKEN])
 
 
 if __name__ == "__main__":
